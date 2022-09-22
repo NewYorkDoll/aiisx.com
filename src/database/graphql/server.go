cd28da2616cd2a4176dd8d890045dc5801c10ce3
NewYorkDoll <yiziluoying@gmail.com>
2022-09-22 14:17:13 +0800

    add: 联调前后端graphql请求

--- src/database/graphql/server.go
+++ src/database/graphql/server.go
@@ -1,12 +1,14 @@
 package graphql
 
 import (
+	"net/http"
 	"os"
 
 	"aiisx.com/src/database/graphql/graph"
 	"aiisx.com/src/database/graphql/graph/generated"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
 )
 
 const defaultPort = "8888"
@@ -21,8 +23,8 @@ func New() *handler.Server {
 	srv.AddTransport(transport.Options{})
 	srv.AddTransport(transport.POST{})
 	srv.AddTransport(transport.GET{})
-	// http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	// http.Handle("/", srv)
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
 
 	// log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	// log.Fatal(http.ListenAndServe(":"+port, nil))
