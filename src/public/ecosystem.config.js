08551c80c768f35a3c4f5fe1e30bee4a425a1c9f
NewYorkDoll <yiziluoying@gmail.com>
2023-01-19 23:12:34 +0800

    add pm2 && package-lock.json

---
+++ src/public/ecosystem.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  apps: [
+    {
+      name: 'NuxtAppName',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './.output/server/index.mjs'
+    }
+  ]
+}
+
